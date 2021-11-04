9016a760750a9b1ccee3f186996cf96f4568aa3b
wood-len <54560238+wood-len@users.noreply.github.com>
2021-11-04 09:26:47 -0600

    created and routed calendar component

--- AngularSchedulingApp/src/app/app-routing.module.ts
+++ AngularSchedulingApp/src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { FormComponent } from './components/form/form.component';
+import { CalendarComponent } from './components/calendar/calendar.component';
 
 const routes: Routes = [
   {path: 'form', component: FormComponent },
-  {path: '', redirectTo: '/form', pathMatch: 'full'}
+  {path: '', redirectTo: '/form', pathMatch: 'full'},
+  {
+    path: "calendar",
+    component: CalendarComponent
+  },
 ];
 
 @NgModule({
