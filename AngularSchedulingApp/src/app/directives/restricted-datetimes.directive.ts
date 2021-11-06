6df77f78a2d9125ce1fbd4cabac301cbbb72b645
ptrcktylr <ptrcktylr@icloud.com>
2021-11-06 09:38:51 -0400

    commented directive, added spacing

--- AngularSchedulingApp/src/app/directives/restricted-datetimes.directive.ts
+++ AngularSchedulingApp/src/app/directives/restricted-datetimes.directive.ts
@@ -5,12 +5,16 @@ export function startDateValidator() :ValidatorFn {
 
     return (control: AbstractControl) :{[key: string]: boolean} | null => {
 
+        // get the current date and clear the hours
         let currentDate = new Date();
         currentDate.setHours(0,0,0,0);
 
+        // get the input date and make it into a date object
         let inputArr = control.value.split('-');
         let inputDate = new Date(`${inputArr[1]}/${inputArr[2]}/${inputArr[0]}`)
+        inputDate.setHours(0,0,0,0);
 
+        // if input date is before today's date return error
         if (inputDate < currentDate) {
             return {'StartDateBeforeCurrentDate':true};
         }
@@ -22,17 +26,17 @@ export function startDateValidator() :ValidatorFn {
 export function endDateValidator(startDateInput: Date): ValidatorFn {
 
     return (control: AbstractControl) :{[key: string]: boolean} | null => {
-        // starting date
+        // get the input starting date and clear the hours
         let startDate = new Date(startDateInput);
         startDate.setHours(0,0,0,0);
 
-        // ending date
+        // get the input ending date and make it into a date object
         if (control.value) {
             let inputArr = control.value.split('-');
             let inputDate = new Date(`${inputArr[1]}/${inputArr[2]}/${inputArr[0]}`)
             inputDate.setHours(0,0,0,0);
     
-            // if the starting date is before the input ending date
+            // if the starting date is before the input ending date return error
             if (startDate > inputDate) {
                 return {'StartDateAfterEndDate':true};
             }
