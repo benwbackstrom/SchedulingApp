c6f9ae424b93c106e52e45aa000580a7040b5b66
ptrcktylr <ptrcktylr@icloud.com>
2021-11-06 11:51:12 -0400

    added len's code to military-regular time pipe

--- AngularSchedulingApp/src/app/pipes/military-to-regular-time.pipe.ts
+++ AngularSchedulingApp/src/app/pipes/military-to-regular-time.pipe.ts
@@ -6,17 +6,25 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class MilitaryToRegularTimePipe implements PipeTransform {
 
   transform(value: number): string {
-    let amOrPm = "AM";
+    let time="";
+    let ap = "AM";
 
-    if (value > 12) {
-      amOrPm = "PM";
-      return `${value-12} ` + amOrPm;
-    } else if (value == 12) {
-      amOrPm = "PM";
-      return `${value} ` + amOrPm;
-    } else {
-      return `${value} ` + amOrPm
+    // accounts for 12:01PM - 12:59PM
+    if (value > 12 && value < 13) {
+      ap="PM";
+
+    } else if(value > 12) {
+      ap="PM";
+      value -= 12;
     }
+
+    if (value % 1 == 0) {
+      time = value + ":00";
+    }
+    else {
+      time = Math.floor(value) + ":30";
+    }
+    return `${time} ${ap}`;
   }
 
 }
