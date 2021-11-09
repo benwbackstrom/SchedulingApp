4f23d1ab5465955ad0bd34abaf4f8e3abc16feae
benwbackstrom <benwbackstrom@gmail.com>
2021-11-09 13:38:10 -0600

    Imported HttpClientTestingModule to calendar tests

--- AngularSchedulingApp/src/app/services/calendarservice.service.spec.ts
+++ AngularSchedulingApp/src/app/services/calendarservice.service.spec.ts
@@ -1,4 +1,5 @@
 import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 import { CalendarserviceService } from './calendarservice.service';
 
@@ -6,7 +7,9 @@ describe('CalendarserviceService', () => {
   let service: CalendarserviceService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule]
+    });
     service = TestBed.inject(CalendarserviceService);
   });
 
